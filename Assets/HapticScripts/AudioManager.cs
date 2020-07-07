9fd4a93f2b2c21a1c928a368fbfb11db6bb220a3
immersvin <immersv@gmail.com>
2020-07-07 14:30:35 +0530

    Audio Manager added to Hapticscene

---
+++ Assets/HapticScripts/AudioManager.cs
@@ -0,0 +1,43 @@
+﻿
+using UnityEngine;
+using System;
+
+
+[System.Serializable]
+public class Sound
+{
+    public string name;
+    public AudioClip audioClip;
+    [Range(0f,1f)]
+    public float volume;
+    [HideInInspector]
+    public AudioSource audioSource;
+}
+
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    private void Awake()
+    {
+        foreach(Sound s in sounds)
+        {
+            s.audioSource = gameObject.AddComponent<AudioSource>();
+            s.audioSource.clip = s.audioClip;
+            s.audioSource.volume = s.volume;
+        }
+        
+    }
+  
+    public void PlayAudio(string audioName)
+    {
+       Sound s = Array.Find(sounds, sound => sound.name == audioName);
+        if (s == null)
+        {
+            Debug.Log("Audio " + audioName + "not found");
+            return;
+        }
+        s.audioSource.Play();
+    }
+}
--- Assets/HapticScripts/HapticScript.cs
+++ Assets/HapticScripts/HapticScript.cs
@@ -2,18 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+
 public class HapticScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
         Button button = gameObject.GetComponent<Button>();
+        
+        
         button.onClick.AddListener(VibrationFunction);
     }
 
     public void VibrationFunction()
     {
         Debug.Log("Vibrate");
+        
         Handheld.Vibrate();
+        FindObjectOfType<AudioManager>().PlayAudio("Haptic");
     }
 }
