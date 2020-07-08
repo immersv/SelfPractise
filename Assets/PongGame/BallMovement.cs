e5128370cf02c95b19df2e0f73538fcf604b8be2
immersvin <immersv@gmail.com>
2020-07-08 11:34:10 +0530

    Pong Game Created

---
+++ Assets/PongGame/BallMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    public float movementSpeed,increamentSpeed,maxSpeed;
+    int hitCounter = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(StartBall());
+    }
+    public IEnumerator StartBall(bool isplayer1=true)
+    {
+        hitCounter = 0;
+        yield return new WaitForSeconds(2f);
+        if (isplayer1)
+        {
+            BallMove(new Vector2(-1, 0));
+        }
+        else
+        {
+            BallMove(new Vector2(1, 0));
+        }
+
+
+    }
+
+   public void BallMove(Vector2 dir)
+    {
+        dir = dir.normalized;
+        float speed = movementSpeed + hitCounter * increamentSpeed;
+        Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
+        rigidbody.velocity = dir * speed;
+
+    }
+    public void IncrementBallSpeed()
+    {
+        if (hitCounter * increamentSpeed <= maxSpeed)
+        {
+            hitCounter++;
+
+        }
+    }
+}
---
+++ Assets/PongGame/RacketMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RacketMovement : MonoBehaviour
+{
+    public float racketMovementSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float value = Input.GetAxisRaw("Vertical");
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, value) * racketMovementSpeed;
+    }
+}
